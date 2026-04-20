cec93e17c9b1e9d7f2059d648f4e415008bfae5f
Hut <huttofu2288@gmail.com>
2026-04-20 21:28:07 +0700

    Add Lab13

--- Lab11/myproject/myapp/views.py
+++ Lab11/myproject/myapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from myapp.models import Person
+from django.db.models import Q
 
 def index(request):
     all_Person = Person.objects.all()
+    query = request.GET.get('q')
+    if query:
+        all_Person = all_Person.filter(Q(name__icontains=query) | Q(age__icontains=query))
     return render(request, "index.html", {"all_person": all_Person})
 
 def about(request):
