e7dfc072023037efe963173aee8ed43968514667
Hut <huttofu2288@gmail.com>
2026-04-20 21:20:18 +0700

    Add Lab12

--- Lab11/myproject/myapp/views.py
+++ Lab11/myproject/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from myapp.models import Person
 
@@ -11,18 +11,29 @@ def about(request):
 
 def form(request):
     if request.method == "POST":
-        # รับข้อมูลจากฟอร์ม
         name = request.POST.get("name")
         age = request.POST.get("age")
-        # บันทึกลงฐานข้อมูล
-        Person.objects.create(
-            name=name,
-            age=age
-        )
-        # redirect กลับหน้าแรก
+        Person.objects.create(name=name, age=age)
         return redirect("/")
     else:
         return render(request, 'form.html')
+
+def edit(request, person_id):
+    person = get_object_or_404(Person, pk=person_id)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        person.name = name
+        person.age = age
+        person.save()
+        return redirect("/")
+    else:
+        return render(request, "edit.html", {"person": person})
+
+def delete(request, person_id):
+    person = get_object_or_404(Person, pk=person_id)
+    person.delete()
+    return redirect("/")
 
 def contact(request):
     return HttpResponse("<h1>ติดต่อ 68115415 ณัฐวัสส์ ปริญญาประเสริฐ</h1>")
